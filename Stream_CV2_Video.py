faf9359099c6d7cc29ddf79f1f9c155023cc0e3f
José Antonio Taquía Gutiérrez <jose_taquia@hotmail.com>
2021-01-08 12:30:53 -0500

    Add files via upload

---
+++ Stream_CV2_Video.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  7 20:45:26 2021
+
+@author: Intel
+"""
+import cv2
+import streamlit as st
+
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
+    
